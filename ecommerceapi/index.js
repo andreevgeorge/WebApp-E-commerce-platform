235e6e720810ea7a522b58b71508fd5bd90296c5
Georgy <andreevgeorge@yandex.ru>
2021-11-20 21:22:00 +0100

    Auth

--- ecommerceapi/index.js
+++ ecommerceapi/index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
+
+
+
 
 dotenv.config()
+
 mongoose
 .connect(process.env.MONGO_URL)
 .then(()=> console.log("DB Connection Successful!"))
@@ -11,10 +20,15 @@ mongoose
  console.log(err)
 });
 
-app.get('/api/test', ()=>{
- console.log('test is successful');
-})
+app.use(express.json())
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/orders', orderRoute);
+
 
 app.listen(process.env.PORT || 5000, ()=>{
  console.log("Backend is running!");
 })
+ 
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
 const App = () => {
- return <Cart/>
+ return <Register/>
 };
 
 export default App;
